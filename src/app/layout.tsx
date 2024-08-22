4ed80b6b4a323f1842f7f79464d140f1eca13383
BEELY39 <daniel.poubelle.39@gmail.com>
2024-08-22 19:05:55 +0200

    ajoute de referencement google gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleTagManager from "@/components/component/googletagManager";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <GoogleTagManager />
+      <body className={inter.className}>{children}
+        <noscript>
+          <iframe src={`https://www.googletagmanager.com/ns.html?id=GTM-TFSKCSDC`}
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+      </body>
     </html>
   );
 }
